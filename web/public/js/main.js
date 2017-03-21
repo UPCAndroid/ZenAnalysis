6766251470b2e5927b04f7554b3543c16f0293c8
wulei123 <1534563365@qq.com>
2017-03-21 18:34:41 +0800

    web version js logic finished basically

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -1,3 +1,40 @@
-function GetRet(Opt){
-	return Math.round( Math.random() * Opt );
+var addBtn = document.getElementById('addSelect');
+var getBtn = document.getElementById('getSelect');
+function GetRet(Opt) {
+    return Math.round(Math.random() * Opt);
 }
+
+function createSelect(type, text) {
+    var el = document.createElement(type);
+    var textNode = document.createTextNode(text);
+    el.appendChild(textNode);
+    return el;
+}
+
+function main() {
+    var selectArea = document.getElementById('selectArea');
+    var count = 0;
+    addBtn.addEventListener('click', function() {
+        var selectStr = document.getElementById('selectInput').value;
+        if (selectStr) {
+            selectStr = count+ 1 + '. ' + selectStr;
+            selectArea.appendChild(createSelect('div', selectStr));
+            count++;
+            console.log(selectArea)
+            document.getElementById('selectInput').value = '';
+        } else {
+            alert('your input can not be empty');
+        }
+
+    });
+    getBtn.addEventListener('click',function () {
+    		alert(GetRet(count)+1)
+    		while(selectArea.children.length>0){
+    			selectArea.children[0].remove();
+    		}
+    		count = 0;
+    })
+
+}
+
+main()
